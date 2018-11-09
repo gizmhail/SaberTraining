6acdb8d032f4039b9a4ef998ddf254a56a14cd62
Sébastien Poivre <gizmhail@gmail.com>
2018-11-09 23:53:44 +0100

    Bug fix to MoveLockedObjectTowardsTarget, and usage of mass in drag speed

--- Assets/Scripts/EnergyMove.cs
+++ Assets/Scripts/EnergyMove.cs
@@ -13,13 +13,35 @@ interface EnergyLockable {
 public class EnergyMove : MonoBehaviour {
     public Rigidbody lockedRigidbody;
     public SteamVR_Input_Sources handType;
+    [Tooltip("Time in seconds before restoring gravity on the locked object")]
     public float delayBeforeRestoringGravity = 1;
+    [Tooltip("Button pressure level separating energy raise and energy drag. If 0, any press will lead to a drag, 1 will always raise")]
     public float energyDragStartPressure = 0.2f;
+
+    [Header("Object to look search settings")]
+    [Tooltip("Size of the search sphere sent in front of the selected hand")]
+    public float searchSphereSize = 0.2f;
+    [Tooltip("Max raycast search distance")]
+    public float maxSearchDistance = 20;
+
+    public bool useAdvanceMovePhysics = true;
+    
+    [Header("Advanced move physics settings")]
+    [Tooltip("Base move speed for an object of mass 1")]
+    public float baseMoveSpeed = 5.0f;
+    [Tooltip("Locked object mass below this level will be considered as this level")]
+    public float minLockedMass = 0.5f;
+    [Tooltip("Minimum flight time (for an object of mass 1)")]
+    public float maxFlightTime = 2.5f;
+    [Tooltip("Maximum flight time (for an object of mass 1)")]
+    public float minFlightTime = 0.2f;
+
 
     private Hand hand;
     private Interactable lockedInteractable;
     private bool lockedInitialGravity;
-    private float initialDistance = 0;
+    private float initialLockedRaiseDistance = 0;
+    private float initialLockedDragDistance = 0;
 
 
     // Use this for initialization
@@ -55,10 +77,11 @@ public class EnergyMove : MonoBehaviour {
             if (energyLevel > energyDragStartPressure)
             {
                 EnergyDrag();
-                UpdateLockDistance();
+                UpdateLockedRaiseDistance();
             }
             else {
                 EnergyRaise();
+                UpdateLockedDragInitialDistance();
             }
         }
     }
@@ -98,7 +121,8 @@ public class EnergyMove : MonoBehaviour {
         lockedInteractable = lockedRigidbody.gameObject.GetComponent<Interactable>();
         lockedInitialGravity = lockedRigidbody.useGravity;
         lockedRigidbody.useGravity = false;
-        UpdateLockDistance();
+        UpdateLockedRaiseDistance();
+        UpdateLockedDragInitialDistance();
         var lockable = lockedRigidbody.GetComponent<EnergyLockable>();
         if (lockable != null) {
             lockable.EnergyLocked(this);
@@ -120,9 +144,14 @@ public class EnergyMove : MonoBehaviour {
 
     }
 
-    void UpdateLockDistance()
+    void UpdateLockedRaiseDistance()
     {
-        initialDistance = Vector3.Distance(LockedObjectSourceTransform().position, HandTargetTransform().position);
+        initialLockedRaiseDistance = Vector3.Distance(LockedObjectSourceTransform().position, HandTargetTransform().position);
+    }
+
+    void UpdateLockedDragInitialDistance()
+    {
+        initialLockedDragDistance = Vector3.Distance(LockedObjectSourceTransform().position, HandTargetTransform().position);
     }
 
     Transform LockedObjectSourceTransform() {
@@ -151,9 +180,8 @@ public class EnergyMove : MonoBehaviour {
     void EnergyRaise() {
         if (lockedRigidbody == null) return;
         var targetTransform = HandTargetTransform();
-        var raisePosition = targetTransform.position + initialDistance * targetTransform.forward;
+        var raisePosition = targetTransform.position + initialLockedRaiseDistance * targetTransform.forward;
         MoveLockedObjectTowardsTarget(raisePosition, targetTransform.rotation);
-        Debug.DrawRay(raisePosition, targetTransform.forward);
     }
 
     void EnergyDrag()
@@ -165,7 +193,7 @@ public class EnergyMove : MonoBehaviour {
 
     void MoveLockedObjectTowardsTarget(Transform targetTransform)
     {
-        MoveLockedObjectTowardsTarget(transform.position, transform.rotation);
+        MoveLockedObjectTowardsTarget(targetTransform.position, targetTransform.rotation);
     }
 
     void MoveLockedObjectTowardsTarget(Vector3 position, Quaternion rotation)
@@ -173,14 +201,22 @@ public class EnergyMove : MonoBehaviour {
         if (lockedRigidbody == null) return;
         var sourceTransform = LockedObjectSourceTransform();
 
-        //Simple version (in case of inability to use Forcemove sources)
-        //Vector3 grabDirection = position - sourceTransform.position;
-        //lockedRigidbody.AddForce(forceCatchFactor * grabDirection);
-
-        float damping = 1.2f;
-        float frequency = 0.4f;
-        lockedRigidbody.AddForceTowards(sourceTransform.position, position, damping, frequency);
-        lockedRigidbody.AddTorqueTowards(sourceTransform.rotation, rotation, damping, frequency);
-        Debug.DrawRay(sourceTransform.position, position - sourceTransform.position);
+        float moveSpeed = baseMoveSpeed / Mathf.Max(minLockedMass, lockedRigidbody.mass);
+        if (!useAdvanceMovePhysics)
+        {
+            //Simple version (in case of inability to use Forcemove sources)
+            Vector3 grabDirection = position - sourceTransform.position;
+            float currentDistance = grabDirection.magnitude;
+            moveSpeed = Mathf.Min(moveSpeed, grabDirection.magnitude);
+            lockedRigidbody.velocity = moveSpeed * grabDirection.normalized;
+        }
+        else {
+            float flightTime = Mathf.Min(maxFlightTime, Mathf.Max(minFlightTime, initialLockedDragDistance / moveSpeed));
+            Debug.Log("Flighttime: " + flightTime);
+            float damping = 1.2f;
+            float frequency = 1.0f/flightTime;
+            lockedRigidbody.AddForceTowards(sourceTransform.position, position, damping, frequency);
+            lockedRigidbody.AddTorqueTowards(sourceTransform.rotation, rotation, damping, frequency);
+        }
     }
 }
