32f905d192d094e3a36f51e11f2155f928d368be
Sébastien Poivre <gizmhail@gmail.com>
2018-11-17 01:23:05 +0100

    Fix velocity and orientation for laser

--- Assets/Scripts/LaserShot.cs
+++ Assets/Scripts/LaserShot.cs
@@ -3,17 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class LaserShot : MonoBehaviour {
+    public float speed = 3;
     Rigidbody rb;
 	// Use this for initialization
 	void Start () {
         rb = GetComponent<Rigidbody>();
-        rb.velocity = transform.forward;
+        rb.velocity = speed*transform.forward;
         Destroy(gameObject, 5);
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = speed * rb.velocity.normalized;
+        transform.rotation = Quaternion.LookRotation(rb.velocity);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         rb.velocity = rb.velocity.magnitude * collision.contacts[0].normal;
+        rb.angularVelocity = Vector3.zero;
         transform.rotation = Quaternion.LookRotation(collision.contacts[0].normal);
     }
+
 }
+
